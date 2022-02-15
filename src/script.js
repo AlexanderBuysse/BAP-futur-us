4e1f47f54214dc0b8a0ff36fedb8302d1ba2b3b7
alexanderbuysse <alexanderbuysse002@gmail.com>
2022-02-15 21:40:59 +0100

    stufff

--- src/script.js
+++ src/script.js
@@ -14,6 +14,7 @@ import imgGraphLeaves from "../static/ui/graph.png"
 import imgGraphWater from "../static/ui/graphWater.png"
 import imgSliderLeaves from "../static/ui/slider.png"
 import imgArrow from "../static/ui/arrow.png"
+import imgInputCut from "../static/ui/inputCut.png"
 
 const gui = new dat.GUI();
 document.querySelector(`.image-container`).innerHTML  = `<img src="${imgHomeButton}" alt="image" width="144" height="87" class="homeButton">`
@@ -314,6 +315,46 @@ const interactionOverview = (nameInteraction) => {
     document.removeEventListener(`click`, handleMoveDocumentTest)
 }
 
+const codeScreen = () => {
+    const overzicht = document.querySelector(`.overzicht`)
+    overzicht.innerHTML=`
+    <article class="overzichtAll">
+      <h2 class="overzichtAll-title">OVERZICHT</h2>
+      <img class="imgInputCut" src="${imgInputCut}" alt="input" width="760" height="80">
+      <img class="imgInputCut" src="${imgInputCut}" alt="input" width="760" height="80">
+      <img class="imgInputCut" src="${imgInputCut}" alt="input" width="760" height="80">
+    </article>
+    <article class="codeAll">
+      <h2 class="codeAll-title">3481</h2> 
+    </article>
+    <article class="infoAll">
+      <h2 class="infAll-title">Tilia Through Time</h2>
+      <p class="infAll-text">is een real-time experience op  Mariahove.
+        Met deze persoonlijke code zie je hoe jouw parameters de gezondheid van de linde zouden beïnvloeden. </p>
+    </article>
+    <article class="uit">
+      <h2 class="uit-title uit-width">TILIA NODIGT JE UIT</h2>
+      <p class="uit-text">
+        Tilia Through Time is een installatie dat aan de hand van licht en geluid het
+        leven van een van de grootste lindebomen van België blood legt. <br>
+        Hoor hoe sappen door de dikke stam van de boom sijpelen om te 
+        communiceren met omliggende flora en zie hoe de kroon kaal wordt door ziekte of ouderdom.
+      </p>
+      <p class="uit-textbold">Bij het ingeven van jouw code ervaar je hoe de boom zou reageren op
+         de omstandigheden die jij op deze site voor haar hebt gecreëerd.</p>
+         <p>Mariahovelaan 2, <br> 
+          9881 Aalter</p>
+      <h3 class="uit-title">18 FEBRUARI <br> 17 uur - 21 uur</h3>
+    </article>`;
+    overzicht.classList.remove('overzichtBlue');
+    overzicht.classList.remove('overzichtRed');
+    if(!overzicht.classList.contains('overzichtCode')) {
+        overzicht.classList.add('overzichtCode');
+    }
+    
+    //document.querySelector(`.code`).textContent = `${leavesComfirmed}${valuesSliderCloud}`;
+}
+
 const handleClickCodeButton = () => {
     if(stopClickLeaves && stopClickClouds) {
         document.querySelector(`.containerOverview`).classList.add('absolute');
@@ -321,7 +362,7 @@ const handleClickCodeButton = () => {
         var modalOVer = document.querySelector(`.myModalOverView`);
         modalOVer.style.display = "grid";
         dontShowUiHome();
-        document.querySelector(`.code`).textContent = `${leavesComfirmed}${valuesSliderCloud}`;
+        codeScreen();
     }
 }
 
